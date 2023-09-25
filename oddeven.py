174176abc925382288376eeb2751ce127fdbfad2
johaanpauljoe <116513441+johaanpauljoe@users.noreply.github.com>
2023-09-25 23:28:37 +0530

    Create oddeven.py

---
+++ oddeven.py
@@ -0,0 +1,13 @@
+def number(a):
+    if(a%2==0):
+        return "even"
+    else:
+        print("odd no")
+        
+        
+        
+        
+b=int(input("Enter a number"))
+
+c=number(b)
+print(c)
